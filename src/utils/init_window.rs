d8e2a5fab7c23c7d43795cad790abcfee6e29912
MaxMavr <maxmavr2005@gmail.com>
2026-05-03 16:56:38 +0300

    add(init_window)

---
+++ src/utils/init_window.rs
@@ -0,0 +1,32 @@
+use minifb::{Window, WindowOptions};
+use display_info::DisplayInfo;
+use crate::utils::pixels_buffer::Buffer;
+
+fn make_window(name: &str, width: usize, height: usize) -> Window {
+    let mut window = Window::new(
+        name, width, height,
+        WindowOptions::default(),
+    )
+    .unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+
+    window.set_target_fps(60);
+
+    window
+}
+
+fn get_screen_size() -> (usize, usize) {
+    let display_infos = DisplayInfo::all().unwrap();
+    let display = &display_infos[0];
+
+    (display.width as usize, display.height as usize)
+}
+
+pub fn init_window(name: &str) -> (Buffer, Window) {
+    let (width, height) = get_screen_size();
+    let window = make_window(name, width, height);
+    let buffer = Buffer::new(width, height);
+
+    (buffer, window)
+}
